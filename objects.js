fe816be004aeb913231cef5c07c869d0c88570d6
Akoth Cathy <catherinececilia22@gmail.com>
2022-01-24 18:55:48 -0800

    More practice

--- objects.js
+++ objects.js
@@ -6,3 +6,13 @@ let bag = {
 };
 
 console.log("this is my nice bag:" ,bag.handbag + "this is the size", bag.size);
+
+
+let num = 5;
+let num1 = 10;
+
+if (num == 2 ){
+    console.log(`cathy`);
+}else{
+    console.log(`tree`);
+}
